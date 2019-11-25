9a193833d1e964e5e9dc2ccf5433d5ea0804e6e1
Isar Ramses <rammses_@live.com.mx>
2019-11-25 03:42:30 -0600

    Set view steps

--- src/js/vci.js
+++ src/js/vci.js
@@ -8,28 +8,6 @@
       end
   end
 */
-let dummyMetaSourceCode = [
-  {token: 'Identificador', string: 'x', priority: null, extra: null},
-  {token: 'Operador relacional', string: '=', priority: null, extra: null},
-  {token: 'Número entero', string: '14', priority: null, extra: null},
-  {token: 'Caracter especial', string: ';', priority: null, extra: null},
-  {token: 'Identificador', string: 'if', priority: null, extra: null},
-  {token: 'Caracter especial', string: '(', priority: null, extra: null},
-  {token: 'Identificador', string: 'x', priority: null, extra: null},
-  {token: 'Operador relacional', string: '<', priority: null, extra: null},
-  {token: 'Número entero', string: '20', priority: null, extra: null},
-  {token: 'Caracter especial', string: ')', priority: null, extra: null},
-  {token: 'Identificador', string: 'then', priority: null, extra: null},
-  {token: 'TBA', string: 'begin', priority: null, extra: null},
-  {token: 'TBA', string: 'write', priority: null, extra: null},
-  {token: 'Caracter especial', string: '(', priority: null, extra: null},
-  {token: 'Identificador', string: 'x', priority: null, extra: null},
-  {token: 'Caracter especial', string: ')', priority: null, extra: null},
-  {token: 'Caracter especial', string: ';', priority: null, extra: null},
-  {token: 'Identificador', string: 'end', priority: null, extra: null},
-  {token: 'Identificador', string: 'end', priority: null, extra: null}
-]
-
 let $VCI = document.getElementById('VCIContent')
 let dummyVCI = ['x', '10', '=', 'x', 'x', '10', '=', 'x', 'x', '10',
 '=sdf', 'x', 'x', '1sefsef0', '=', 'x', '*']
@@ -45,15 +23,16 @@ function drawVCI(VCIToDraw) {
   }
 }
 
-function filterByTokens(vciElement) {
+function createVCI(vciElement) {
   console.log(vciElement);
 }
 
-function startVCIProcess(metaSourceCode = dummyMetaSourceCode) {
-  console.log(metaSourceCode);
+function startVCIProcess() {
+  btnVCI.classList.add('hidden')
+  output2.classList.remove('hidden')
 
   for (let row = 0; row < metaSourceCode.length; row++) {
-    filterByTokens(metaSourceCode[row])
+    createVCI(metaSourceCode[row])
   }
 
   drawVCI(dummyVCI)
