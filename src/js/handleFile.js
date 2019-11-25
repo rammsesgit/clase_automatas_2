445a82d8eca01252aba691c1dea400e06fe4ff0b
Sergio Saucedo <sergio.saucedo10@gmail.com>
2019-11-25 03:13:45 -0600

    Genera lista de tokens

--- src/js/handleFile.js
+++ src/js/handleFile.js
@@ -1,43 +1,180 @@
 let $sourceFile = document.getElementById('sourceFile')
 let $outputs = document.getElementsByClassName('output')
+let metaSourceCode = [];
 
 function evaluar(matriz) {  
-  console.log(matriz);
-
-/*   for (let i = 0; i < matriz.length; i++) {
+  for (let i = 0; i < matriz.length; i++) {
     for (let j = 0; j < matriz[i].length; j++) {
-      console.log(matriz[i][j] + ' $' + i + ' $' + j);
+      let valorActual = matriz[i][j];
       
-      if (identificadores(matriz[i][j]) != null) {
-        console.log('identificadores:' + matriz[i][j]);
-        
+      if (palabrasReservadas(matriz[i][j]) != null) {
+        if (valorActual === 'then') {
+          metaSourceCode.push({
+            token: 'condicional',
+            string: valorActual,
+            priority: null,
+            extra: 1
+          });
+        } else if (valorActual === 'if') {
+          metaSourceCode.push({
+            token: 'condicional',
+            string: valorActual,
+            priority: null,
+            extra: 3
+          });
+        } else if (valorActual === 'repeat') {
+          metaSourceCode.push({
+            token: 'repeat',
+            string: valorActual,
+            priority: null,
+            extra: 4
+          });
+        } else if (valorActual === 'until') {
+          metaSourceCode.push({
+            token: 'repeat',
+            string: valorActual,
+            priority: null,
+            extra: 5
+          });
+        } else if (valorActual === 'while') {
+          metaSourceCode.push({
+            token: 'while',
+            string: valorActual,
+            priority: null,
+            extra: 6
+          });
+        } else if (valorActual === 'do') {
+          metaSourceCode.push({
+            token: 'while',
+            string: valorActual,
+            priority: null,
+            extra: 7
+          });
+        } else if (valorActual === 'write') {
+          metaSourceCode.push({
+            token: 'write',
+            string: valorActual,
+            priority: null,
+            extra: 8
+          });
+        } else if (valorActual === 'end') {
+          metaSourceCode.push({
+            token: 'end',
+            string: valorActual,
+            priority: null,
+            extra: 9
+          });
+        }
+      } else if (identificadores(matriz[i][j]) != null) {
+        metaSourceCode.push({
+          token: 'identificador',
+          string: valorActual,
+          priority: null,
+          extra: null
+        });
       } else if (operadoresAritmeticos(matriz[i][j]) != null) {
-        
-        console.log('operadoresAritmeticos:' + matriz[i][j]);
+        metaSourceCode.push({
+          token: 'aritmetico',
+          string: valorActual,
+          priority: obtenerPrioridad(valorActual),
+          extra: null
+        });
       } else if (operadoresRelacionales(matriz[i][j]) != null) {
-        
-        console.log('operadoresRelacionales:' + matriz[i][j]);
+        metaSourceCode.push({
+          token: 'relacional',
+          string: valorActual,
+          priority: obtenerPrioridad(valorActual),
+          extra: null
+        });
       } else if (operadoresLogicos(matriz[i][j]) != null) {
-        
-        console.log('operadoresLogicos:' + matriz[i][j]);
-      } else if (palabrasReservadas(matriz[i][j]) != null) {
-        
-        console.log('palabrasReservadas:' + matriz[i][j]);
-      } else if (caracteresEspeciales(matriz[i][j]) != null) {
-        
-        console.log('caracteresEspeciales:' + matriz[i][j]);
+        metaSourceCode.push({
+          token: 'logico',
+          string: valorActual,
+          priority: obtenerPrioridad(valorActual),
+          extra: null
+        });
       } else if (comentarios(matriz[i][j]) != null) {
         
-        console.log('comentarios:' + matriz[i][j]);
       } else if (numerosEnteros(matriz[i][j]) != null) {
-        
-        console.log('numerosEnteros:' + matriz[i][j]);
+        metaSourceCode.push({
+          token: 'numero',
+          string: valorActual,
+          priority: null,
+          extra: null
+        });
       } else if (numerosReales(matriz[i][j]) != null) {
-        
-        console.log('numerosReales:' + matriz[i][j]);
+        metaSourceCode.push({
+          token: 'numero',
+          string: valorActual,
+          priority: null,
+          extra: null
+        });
+      } else if (caracteresEspeciales(matriz[i][j]) != null) {
+        if (valorActual === ';') {
+          metaSourceCode.push({
+            token: 'delimitador',
+            string: valorActual,
+            priority: null,
+            extra: 10
+          });
+        } else if (valorActual === '=') {
+          metaSourceCode.push({
+            token: 'asignacion',
+            string: valorActual,
+            priority: 0,
+            extra: 11
+          });
+        } else if (valorActual === '(') {
+          metaSourceCode.push({
+            token: 'abreParentesis',
+            string: valorActual,
+            priority: 0,
+            extra: 12
+          });
+        } else if (valorActual === ')') {
+          metaSourceCode.push({
+            token: 'cierraParentesis',
+            string: valorActual,
+            priority: 0,
+            extra: 13
+          });
+        }
       }
     }
-  } */
+  } 
+}
+
+function obtenerPrioridad(string) {
+  let prioridad = 0;
+
+  switch (string) {
+    case "/":
+      prioridad = 60;
+      break
+    case "*":
+      prioridad = 60;
+      break
+    case "+":
+      prioridad = 50;
+      break
+    case "-":
+      prioridad = 50;
+      break
+    case "<" || ">" || "<=" || ">=" || "==":
+      prioridad = 40;
+      break
+    case "!":
+      prioridad = 30
+      break
+    case "&&":
+      prioridad = 20
+      break
+    case "||":
+      prioridad = 10
+      break
+  }
+
+  return prioridad;
 }
 
 function fragmentarArchivo(fuente) {
@@ -46,8 +183,11 @@ function fragmentarArchivo(fuente) {
   for (let index = 0; index < filas.length; index++) {
     matriz.push(filas[index].split(' '))
   }
+
+  console.log(matriz);
   
-  evaluar(matriz)
+  evaluar(matriz);
+  console.log(metaSourceCode);
 }
 
 function mostrarContenido(contenido) {
