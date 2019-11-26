05d8df146b5f6ec6734a54fa4cc4f5c6c3d170fd
Sergio Saucedo <sergio.saucedo10@gmail.com>
2019-11-26 10:14:07 -0600

    Implementa reglas de ejecucion

--- src/js/executionStack.js
+++ src/js/executionStack.js
@@ -7,6 +7,121 @@ let res = {};
 
 function startPEProcess(vci) {
   for (let i = 0 ; i < vci.length ; i++) {
-    console.log(vci[i].string);
+    let current = vci[i];
+    if (identificadores(current) !== undefined) {
+      if (current === 'then') {
+        // implementar then
+      } else if (current === 'else') {
+        // implementar else
+      } else {
+        eje.push(current);
+      }
+    }
+    if (numerosEnteros(current) !== undefined) {
+      eje.push(current);
+    }
+    if (operadoresAritmeticos(current) !== undefined) {
+      let second = eje.pop();
+      let first = eje.pop();
+      evaluateArithmeticOperator(current, first, second);
+    }
+    if (operadoresRelacionales(current) !== undefined) {
+      let second = eje.pop();
+      let first = eje.pop();
+      evaluateRelationalOperator(current, first, second);
+    }
+    if (operadoresLogicos(current) !== undefined) {
+      if (current === '~') {
+        evaluateLogicalOperator(current, eje.pop(), null)
+      } else {
+        let second = eje.pop();
+        let first = eje.pop();
+        evaluateLogicalOperator(current, first, second);
+      }
+    }
+    if (current === '=') {
+      let value = eje.pop();
+      let identifier = eje.pop();
+      res[identifier] = value;
+    }
+  }
+}
+
+function evaluateArithmeticOperator(operator, first, second) {
+  if (numerosEnteros(first) && numerosEnteros(second)) {
+    switch (operator) {
+      case "/":
+        eje.push(first/second);
+        break;
+      case "*":
+        eje.push(first*second);
+        break;
+      case "+":
+        eje.push(first+second);
+        break;
+      case "-":
+        eje.push(first-second);
+        break;
+    }
+  } else {
+    console.log('No se puede realizar la operación [aritmético] [' + first + ' ' + operator + ' ' + second + ']')
+  }
+}
+
+function evaluateRelationalOperator(operator, first, second) {
+  if (operator === '>') {
+    if (identificadores(first) !== undefined && numerosEnteros(second) !== undefined) {
+      eje.push(res[first] > second);
+    } else if (numerosEnteros(first) !== undefined && identificadores(second) !== undefined) {
+      eje.push(first > res[second]);
+    } else {
+      eje.push(first > second);
+    }
+  } else if (operator === '<') {
+    if (identificadores(first) !== undefined && numerosEnteros(second) !== undefined) {
+      eje.push(res[first] < second);
+    } else if (numerosEnteros(first) !== undefined && identificadores(second) !== undefined) {
+      eje.push(first < res[second]);
+    } else {
+      eje.push(first < second);
+    }
+  } else if (operator === '<=') {
+    if (identificadores(first) !== undefined && numerosEnteros(second) !== undefined) {
+      eje.push(res[first] <= second);
+    } else if (numerosEnteros(first) !== undefined && identificadores(second) !== undefined) {
+      eje.push(first <= res[second]);
+    } else {
+      eje.push(first <= second);
+    }
+  } else if (operator === '>=') {
+    if (identificadores(first) !== undefined && numerosEnteros(second) !== undefined) {
+      eje.push(res[first] >= second);
+    } else if (numerosEnteros(first) !== undefined && identificadores(second) !== undefined) {
+      eje.push(first >= res[second]);
+    } else {
+      eje.push(first >= second);
+    }
+  } else if (operator === '==') {
+    if (identificadores(first) !== undefined && numerosEnteros(second) !== undefined) {
+      eje.push(res[first] == second);
+    } else if (numerosEnteros(first) !== undefined && identificadores(second) !== undefined) {
+      eje.push(first == res[second]);
+    } else {
+      eje.push(first == second);
+    }
+  } else {
+    console.log('No se puede realizar la operación [relacional] [' + first + ' ' + operator + ' ' + second + ']');
+  }
+}
+
+function evaluateLogicalOperator(operator, first, second) {
+  if (operator === '&') {
+    eje.push(first && second);
+  } else if (operator === '|') {
+    eje.push(first || second);
+  } else if (operator === '~' && second === null) {
+    eje.push(!first)
+  } else {
+    console.log('No se puede realizar la operación [lógica] [' + first + ' ' + operator + ' ' + second + ']');
   }
 }
